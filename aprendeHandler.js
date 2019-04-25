b3a27545fd4568e6a28067da3af6b968ec68165f
Andresrodart <arodartel1601@alumno.ipn.mx>
2019-04-25 00:06:38 -0500

    index

--- aprendeHandler.js
+++ aprendeHandler.js
@@ -5,20 +5,49 @@ class Aprender {
 		this.informacion = informacion;
 		this.estado = 'inicial';
 		this.temasDados = [];
+		this.informacionLen = this.informacion[this.tema].length;
 	}
 	maquinaDeEstados(){
 		if (this.estado === 'inicial')
 			return this.inicial();
+		else if (this.estado === 'continuar')
+			return this.continuar();
 	}
 	inicial(){
-		let TemaADar = Math.floor(Math.random() * this.informacion[this.tema].length);
-		this.temasDados.push(TemaADar);
-		return `<speak> 
+		let TemaADar = Math.floor(Math.random() * this.informacionLen);
+		let myResponse = `<speak> 
 					Vamos a empezar <break time="1s"/>.
 					Te contaré sobre <emphasis level="reduced">${this.informacion[this.tema][TemaADar].titulo}</emphasis> <break time="1s"/>.
-					${this.informacion[this.tema][TemaADar].catedra} <break time="1s"/> 
-					Si quieres continuar con otra pregunta di continuar o no continuar
-				</speak>`
+					${this.informacion[this.tema][TemaADar].catedra}. <break time="1s"/> 
+					Quieres saber un poco más de información di continuar o no continuar
+				</speak>`;
+		let tempSubTema = this.informacion[this.tema][TemaADar];
+		let lastSubTema = this.informacion[this.tema][this.informacionLen - 1];
+		this.informacion[this.tema][TemaADar] = lastSubTema;
+		this.informacion[this.tema][this.informacionLen - 1] = tempSubTema;
+		this.informacionLen--;
+		
+		return myResponse;
+	}
+	continuar(){
+		if (this.informacion <= 0)
+			return `<speak> 
+						Ya te he enseñado todo lo que se
+					</speak>`
+			let TemaADar = Math.floor(Math.random() * this.informacionLen);
+			let myResponse = `<speak> 
+						Siguiente tema padawan <break time="1s"/>.
+						Te contaré sobre <emphasis level="reduced">${this.informacion[this.tema][TemaADar].titulo}</emphasis> <break time="1s"/>.
+						${this.informacion[this.tema][TemaADar].catedra}. <break time="1s"/> 
+						Quieres saber un poco más de información di continuar o no continuar
+					</speak>`;
+			let tempSubTema = this.informacion[this.tema][TemaADar];
+			let lastSubTema = this.informacion[this.tema][this.informacionLen - 1];
+			this.informacion[this.tema][TemaADar] = lastSubTema;
+			this.informacion[this.tema][this.informacionLen - 1] = tempSubTema;
+			this.informacionLen--;
+			
+			return myResponse;
 	}
 }
 
--- index.js
+++ index.js
@@ -7,126 +7,129 @@ const Aprender = require('./aprenderHandler.js')
 const Alexa = require('ask-sdk-core');
 const WelcomeDialogs = ['¿Estás aquí para retarme, o estas aquí para recivir lecciones de mamá Alexa?', '¿Acaso tu Kunfu es más fuerte, o quieres entrenar?', 'Vaya, veo que hay un retador entre nosotros, ¿estas listo o necesitas ayuda?'];
 const LaunchRequestHandler = {
-    canHandle(handlerInput) {
-        return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
-    },
-    handle(handlerInput) {
-        const speechText = WelcomeDialogs[Math.floor(Math.random() * WelcomeDialogs.length)];
-        return handlerInput.responseBuilder
-            .speak(speechText)
-            .reprompt(speechText)
-            .getResponse();
-    }
+	canHandle(handlerInput) {
+		return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
+	},
+	handle(handlerInput) {
+		const speechText = WelcomeDialogs[Math.floor(Math.random() * WelcomeDialogs.length)];
+		return handlerInput.responseBuilder
+			.speak(speechText)
+			.reprompt(speechText)
+			.getResponse();
+	}
 };
 
 const AprendeIntent = {
-    canHandle(handlerInput){
-        return  handlerInput.requestEnvelope.request.type === 'IntentRequest' 
-        && handlerInput.requestEnvelope.request.intent.name === 'AprendeIntent';
-    },
-    handle(handlerInput) {
-        const request = handlerInput.requestEnvelope.request;
-        let myResponse = "Houston hubo un problema";
-                var tema = request.intent.slots.tema.resolutions.resolutionsPerAuthority[0].values[0].value.id;
-                var nivel = request.intent.slots.nivel.resolutions.resolutionsPerAuthority[0].values[0].value.name;
-                informacion = require(`./nivel/${nivel}.json`);
-                teacher = new Aprender(nivel, tema, informacion);
-                myResponse = teacher.maquinaDeEstados();
-        return handlerInput.responseBuilder
-            .speak(myResponse)
-            .reprompt('Si quieres continuar con otra pregunta di continuar o no continuar')
-            .getResponse();
-    }
+	canHandle(handlerInput){
+		return  handlerInput.requestEnvelope.request.type === 'IntentRequest' 
+		&& handlerInput.requestEnvelope.request.intent.name === 'AprendeIntent';
+	},
+	handle(handlerInput) {
+		const request = handlerInput.requestEnvelope.request;
+		let myResponse = "Houston hubo un problema";
+				var tema = request.intent.slots.tema.resolutions.resolutionsPerAuthority[0].values[0].value.id;
+				var nivel = request.intent.slots.nivel.resolutions.resolutionsPerAuthority[0].values[0].value.name;
+				informacion = require(`./nivel/${nivel}.json`);
+				teacher = new Aprender(nivel, tema, informacion);
+				myResponse = teacher.maquinaDeEstados();
+		return handlerInput.responseBuilder
+			.speak(myResponse)
+			.reprompt('Quieres saber un poco más de información di continuar o no continuar')
+			.getResponse();
+	}
 } 
 
 const ContinuarIntent = {
-    canHandle(handlerInput){
-        return  handlerInput.requestEnvelope.request.type === 'IntentRequest' 
-        && handlerInput.requestEnvelope.request.intent.name === 'ContinuarIntent';
-    },
-    handle(handlerInput) {
-        const request = handlerInput.requestEnvelope.request;
-        let myResponse = 'Houston tenemos un problema';
-       if(teacher !== null){
-            var Qcontinuar = request.intent.slots.continuar.resolutions.resolutionsPerAuthority[0].values[0].value.id;
-            myResponse = toString(Qcontinuar);
-       }else{
-           //Sigue preguntando
-       }
-        return handlerInput.responseBuilder
-            .speak(myResponse)
-            .reprompt(myResponse)
-            .getResponse();
-    }
+	canHandle(handlerInput){
+		return  handlerInput.requestEnvelope.request.type === 'IntentRequest' 
+		&& handlerInput.requestEnvelope.request.intent.name === 'ContinuarIntent';
+	},
+	handle(handlerInput) {
+		const request = handlerInput.requestEnvelope.request;
+		let Qcontinuar = request.intent.slots.continuar.value;
+		let myResponse = 'Hosuton no hizimos nada'
+	   if(teacher !== null){
+		   if (Qcontinuar.toLowerCase() === 'continuar') {
+				teacher.estado = 'continuar';
+				myResponse = teacher.maquinaDeEstados();
+			}
+	   }else{
+			myResponse = 'Sigue';
+	   }
+		return handlerInput.responseBuilder
+			.speak(myResponse)
+			.reprompt(myResponse)
+			.getResponse();
+	}
 }
 
 const RetoIntent = {
-    canHandle(handlerInput){
-        return  handlerInput.requestEnvelope.request.type === 'IntentRequest' 
-        && handlerInput.requestEnvelope.request.intent.name === 'RetoIntent';
-    },
-    handle(handlerInput) {
-        const request = handlerInput.requestEnvelope.request;
-        
-        //var tema = request.intent.slots.tema.resolutions.resolutionsPerAuthority[0].values[0].value.name;
-        //var nivel = request.intent.slots.nivel.resolutions.resolutionsPerAuthority[0].values[0].value.name;
-      
-        return handlerInput.responseBuilder
-            .speak('aqui me retas')
-            .reprompt('aqui me retas')
-            .getResponse();
-    }
+	canHandle(handlerInput){
+		return  handlerInput.requestEnvelope.request.type === 'IntentRequest' 
+		&& handlerInput.requestEnvelope.request.intent.name === 'RetoIntent';
+	},
+	handle(handlerInput) {
+		const request = handlerInput.requestEnvelope.request;
+		
+		//var tema = request.intent.slots.tema.resolutions.resolutionsPerAuthority[0].values[0].value.name;
+		//var nivel = request.intent.slots.nivel.resolutions.resolutionsPerAuthority[0].values[0].value.name;
+	  
+		return handlerInput.responseBuilder
+			.speak('aqui me retas')
+			.reprompt('aqui me retas')
+			.getResponse();
+	}
 } 
 
 
 const HelloWorldIntentHandler = {
-    canHandle(handlerInput) {
-        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-            && handlerInput.requestEnvelope.request.intent.name === 'HelloWorldIntent';
-    },
-    handle(handlerInput) {
-        const speechText = 'Hello World!';
-        return handlerInput.responseBuilder
-            .speak(speechText)
-            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
-            .getResponse();
-    }
+	canHandle(handlerInput) {
+		return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+			&& handlerInput.requestEnvelope.request.intent.name === 'HelloWorldIntent';
+	},
+	handle(handlerInput) {
+		const speechText = 'Hello World!';
+		return handlerInput.responseBuilder
+			.speak(speechText)
+			//.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+			.getResponse();
+	}
 };
 const HelpIntentHandler = {
-    canHandle(handlerInput) {
-        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-            && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.HelpIntent';
-    },
-    handle(handlerInput) {
-        const speechText = 'You can say hello to me! How can I help?';
+	canHandle(handlerInput) {
+		return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+			&& handlerInput.requestEnvelope.request.intent.name === 'AMAZON.HelpIntent';
+	},
+	handle(handlerInput) {
+		const speechText = 'You can say hello to me! How can I help?';
 
-        return handlerInput.responseBuilder
-            .speak(speechText)
-            .reprompt(speechText)
-            .getResponse();
-    }
+		return handlerInput.responseBuilder
+			.speak(speechText)
+			.reprompt(speechText)
+			.getResponse();
+	}
 };
 const CancelAndStopIntentHandler = {
-    canHandle(handlerInput) {
-        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-            && (handlerInput.requestEnvelope.request.intent.name === 'AMAZON.CancelIntent'
-                || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent');
-    },
-    handle(handlerInput) {
-        const speechText = 'Goodbye!';
-        return handlerInput.responseBuilder
-            .speak(speechText)
-            .getResponse();
-    }
+	canHandle(handlerInput) {
+		return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+			&& (handlerInput.requestEnvelope.request.intent.name === 'AMAZON.CancelIntent'
+				|| handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent');
+	},
+	handle(handlerInput) {
+		const speechText = 'Goodbye!';
+		return handlerInput.responseBuilder
+			.speak(speechText)
+			.getResponse();
+	}
 };
 const SessionEndedRequestHandler = {
-    canHandle(handlerInput) {
-        return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest';
-    },
-    handle(handlerInput) {
-        // Any cleanup logic goes here.
-        return handlerInput.responseBuilder.getResponse();
-    }
+	canHandle(handlerInput) {
+		return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest';
+	},
+	handle(handlerInput) {
+		// Any cleanup logic goes here.
+		return handlerInput.responseBuilder.getResponse();
+	}
 };
 
 // The intent reflector is used for interaction model testing and debugging.
@@ -134,52 +137,52 @@ const SessionEndedRequestHandler = {
 // for your intents by defining them above, then also adding them to the request
 // handler chain below.
 const IntentReflectorHandler = {
-    canHandle(handlerInput) {
-        return handlerInput.requestEnvelope.request.type === 'IntentRequest';
-    },
-    handle(handlerInput) {
-        const intentName = handlerInput.requestEnvelope.request.intent.name;
-        const speechText = `You just triggered ${intentName}`;
+	canHandle(handlerInput) {
+		return handlerInput.requestEnvelope.request.type === 'IntentRequest';
+	},
+	handle(handlerInput) {
+		const intentName = handlerInput.requestEnvelope.request.intent.name;
+		const speechText = `You just triggered ${intentName}`;
 
-        return handlerInput.responseBuilder
-            .speak(speechText)
-            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
-            .getResponse();
-    }
+		return handlerInput.responseBuilder
+			.speak(speechText)
+			//.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+			.getResponse();
+	}
 };
 
 // Generic error handling to capture any syntax or routing errors. If you receive an error
 // stating the request handler chain is not found, you have not implemented a handler for
 // the intent being invoked or included it in the skill builder below.
 const ErrorHandler = {
-    canHandle() {
-        return true;
-    },
-    handle(handlerInput, error) {
-        console.log(`~~~~ Error handled: ${error.message}`);
-        const speechText = `Sorry, I couldn't understand what you said. Please try again.`;
+	canHandle() {
+		return true;
+	},
+	handle(handlerInput, error) {
+		console.log(`~~~~ Error handled: ${error.message}`);
+		const speechText = `Sorry, I couldn't understand what you said. Please try again.`;
 
-        return handlerInput.responseBuilder
-            .speak(speechText)
-            .reprompt(speechText)
-            .getResponse();
-    }
+		return handlerInput.responseBuilder
+			.speak(speechText)
+			.reprompt(speechText)
+			.getResponse();
+	}
 };
 
 // This handler acts as the entry point for your skill, routing all request and response
 // payloads to the handlers above. Make sure any new handlers or interceptors you've
 // defined are included below. The order matters - they're processed top to bottom.
 exports.handler = Alexa.SkillBuilders.custom()
-    .addRequestHandlers(
-        LaunchRequestHandler,
-        HelloWorldIntentHandler,
-        HelpIntentHandler,
-        AprendeIntent,
-        RetoIntent,
-        ContinuarIntent,
-        CancelAndStopIntentHandler,
-        SessionEndedRequestHandler,
-        IntentReflectorHandler) // make sure IntentReflectorHandler is last so it doesn't override your custom intent handlers
-    .addErrorHandlers(
-        ErrorHandler)
-    .lambda();
+	.addRequestHandlers(
+		LaunchRequestHandler,
+		HelloWorldIntentHandler,
+		HelpIntentHandler,
+		AprendeIntent,
+		RetoIntent,
+		ContinuarIntent,
+		CancelAndStopIntentHandler,
+		SessionEndedRequestHandler,
+		IntentReflectorHandler) // make sure IntentReflectorHandler is last so it doesn't override your custom intent handlers
+	.addErrorHandlers(
+		ErrorHandler)
+	.lambda();
